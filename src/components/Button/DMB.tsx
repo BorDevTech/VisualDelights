9841c8bab27c3ac575f0bba91422c2d6777b73e5
BorDevTech <bordevtech@icloud.com>
2024-06-14 18:11:22 -0400

    completed DMB(darkmode button)

--- src/components/Button/DMB.tsx
+++ src/components/Button/DMB.tsx
@@ -1,13 +1,27 @@
 "use client";
 
-import { Button } from "@chakra-ui/react";
+import { Button, IconButton } from "@chakra-ui/react";
 import { useColorMode } from "@chakra-ui/react";
+import { MdBrightness2, MdBrightness5 } from "react-icons/md";
 
 export default function DMB() {
   const { colorMode, toggleColorMode } = useColorMode();
   return (
-    <Button onClick={toggleColorMode}>
+    <IconButton
+      p={"20px 20px"}
+      m={"0px 40px"}
+      onClick={toggleColorMode}
+      aria-label="Darkmode Switch"
+      icon={colorMode === "dark" ? <MdBrightness2 /> : <MdBrightness5 />}
+      variant={`solid`}
+      bgColor={"purple.700"}
+      color={"pink"}
+      _hover={{
+        bgColor: "purple.500",
+        color: "pink",
+      }}
+    >
       Toggle {colorMode === "light" ? "Dark" : "Light"}
-    </Button>
+    </IconButton>
   );
 }
