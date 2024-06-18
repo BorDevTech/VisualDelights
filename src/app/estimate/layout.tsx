f4075f32320ef4dc5a00493a15ca1b53012d9989
BorDevTech <bordevtech@icloud.com>
2024-06-17 22:06:23 -0400

    test 2

--- src/app/estimate/layout.tsx
+++ src/app/estimate/layout.tsx
@@ -1,11 +1,11 @@
 import Navbar from "@/components/Navbar/navbar";
 import React from "react";
-// import type { Metadata } from "next";
+import type { Metadata } from "next";
 
-// export const metadata: Metadata = {
-//   title: "Estimate",
-//   description: "Site created by BorDevTech",
-// };
+export const metadata: Metadata = {
+  title: "Estimate",
+  description: "Site created by BorDevTech",
+};
 
 export default function Layout({
   children,
