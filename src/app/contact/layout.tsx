5c18860d65f2c9fdc3bd3392076bd03ddb92ddb3
Danielle Robinson <danielle.robinson@bordvetech.com>
2024-06-22 14:38:12 -0400

    Contact Page

---
+++ src/app/contact/layout.tsx
@@ -0,0 +1,16 @@
+import Navbar from "@/components/Navbar/navbar";
+import React from "react";
+import type { Metadata } from "next";
+
+export const metadata:Metadata = {title:"Visual Delights | Contact"}
+
+const Layout = ({ children }: Readonly<{ children: React.ReactNode }>) => {
+  return (
+    <>
+      <Navbar />
+      {children}
+    </>
+  );
+};
+
+export default Layout;
