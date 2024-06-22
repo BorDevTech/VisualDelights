e83243845d00cf8218867e67bfb2ae78fe553988
BorDevTech <bordevtech@icloud.com>
2024-06-22 12:49:56 -0400

    GTag attempt 3

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -5,6 +5,7 @@ import { ColorModeScript, theme } from "@chakra-ui/react";
 import ClarityAnalytics from "./clarity";
 import GoogleAnalytics from "./googleAnalytics";
 import { GoogleTagManager } from "@next/third-parties/google";
+import Head from "next/head";
 
 export const metadata: Metadata = {
   title: { template: "Visual Delights | %s", default: `Visual Delights` },
@@ -18,13 +19,20 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
+      <Head>
+        <meta
+          name="google-site-verification"
+          content="YTF_mPpBAm2hGN6VZvdSSKwEuUpjbGQHVv-3gguO9Eo"
+        />
+        <GoogleAnalytics GoogleTrackingID={"G-16SCX6MGYD"} />
+
+        <GoogleTagManager gtmId="GTM-TNMZ283Q" />
+        <ClarityAnalytics ClarityTrackingID={"mupdzb93g6"} />
+      </Head>
+
       <body
       // className={inter.className}
       >
-        <ClarityAnalytics ClarityTrackingID={"mupdzb93g6"} />
-
-        <GoogleTagManager gtmId="GTM-TNMZ283Q" />
-        <GoogleAnalytics GoogleTrackingID={"G-16SCX6MGYD"} />
         <ColorModeScript initialColorMode={theme.config.initialColorMode} />
         <Provider>{children}</Provider>
       </body>
